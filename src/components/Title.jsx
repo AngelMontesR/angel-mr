2edddd4fc1b872d575b822cb7e6c798f2a8d140e
Angel Montes <angelmontes963@gmail.com>
2025-10-18 20:15:34 -0600

    feat: reemplazar encabezados por el componente Title en las secciones Acerca de, Portafolio y Habilidades

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+export default function Title({ title }) {
+    return (
+        <h2
+        class="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-bl from-green-400 to-indigo-700"
+      >
+        {title}
+      </h2>
+    );
+}
