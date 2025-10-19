d3431da6fb2ffe55fd13f9a730784abfa127fceb
Angel Montes <angelmontes963@gmail.com>
2025-10-18 19:44:13 -0600

    feat: mejorar la presentación del componente CardPortafolio y corregir la ruta de la imagen

--- src/components/CardPortafolio.jsx
+++ src/components/CardPortafolio.jsx
@@ -4,17 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function CardPortafolio() {
     return (
-        <div className="relative w-full h-72 rounded-xl shadow-lg shadow-indigo-900 overflow-hidden group">
+        <div className="relative w-full h-72 rounded-xl shadow-lg shadow-indigo-900 overflow-hidden">
             <img
-                src="/public/img/desk.jpg"
+                src="/img/desk.jpg"
                 alt="Project 1"
                 className="w-full h-full object-cover transition-transform"
             />
-            {/* Overlay solo en la mitad inferior */}
-            <div className="absolute bottom-0 left-0 w-full h-1/2 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-400 flex flex-col justify-end p-5">
+            <div className="absolute bottom-0 left-0 w-full h-1/2 bg-indigo-950 bg-opacity-50 flex flex-col justify-end p-5">
                 <h3 className="text-white text-xl font-bold mb-2">Project One</h3>
                 <p className="text-gray-300 mb-4 text-sm">
                     A brief description of Project One. Resalta lo más importante.
+                    <span className="underline cursor-pointer ml-2 text-green-400 hover:text-green-600">
+                        Ver más
+                    </span>
                 </p>
                 <div className="flex space-x-4">
                     <a
@@ -34,7 +36,5 @@ export default function CardPortafolio() {
                 </div>
             </div>
         </div>
-
-
     );
 }
